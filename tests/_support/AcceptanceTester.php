e6964a1ef9e64c96d925ec4850c4d6a76cda1842
Joseph Lacey <joseph@palantetech.coop>
2017-05-12 11:13:06 -0400

    Updates to AcceptanceTester config

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -18,7 +18,7 @@
 */
 class AcceptanceTester extends \Codeception\Actor
 {
-    use _generated\AcceptanceTesterActions;
+  use _generated\AcceptanceTesterActions;
 
    /**
     * Login & logout actions
